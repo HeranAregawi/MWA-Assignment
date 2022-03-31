f6df44dfb687e8144f79be0583fdbdb7612adfa4
Heran Aregawi <heranaregawi@gmail.com>
2022-03-31 07:26:32 -0500

    assignment2 file name changed

--- Assignment_02/question02/app.js
+++ Assignment_02/question02/app.js
@@ -2,25 +2,18 @@ const express = require("express");
 const path = require("path");
 const s=require("dotenv").config();
 const app = express();
+
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", function(req, res){
+
+app.get("/", function(req, res){ß
     console.log("Get Received");
     res.status(200).sendFile(path.join(__dirname, "public ","index.html"));
 })
 
-
-app.get("/page1", function(req, res){
-    console.log("Get Received");
-    res.status(200).sendFile(path.join(__dirname, "public ","page1.html"));
-})
-app.get("/page2", function(req, res){
-    console.log("Get Received");
-    res.status(200).sendFile(path.join(__dirname, "public ","page2.html"));
-})
 app.post("/json", function(req, res){
     console.log("Json Received");
-    res.status(200).json({"JSON_Data" : true});
+    res.status(200).sendFile(path.join(__dirname, "public", "json"));
 })
 
 
