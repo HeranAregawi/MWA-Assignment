eca97e76eed8d1ef774c3bf74f3229c894a67ae0
Heran Aregawi <heranaregawi@gmail.com>
2022-03-30 21:18:11 -0500

    question3 added

--- Assignment_03/question2/api/controllers/add.controller.js
+++ Assignment_03/question2/api/controllers/add.controller.js
@@ -3,9 +3,10 @@ module.exports.gamesGetAll= function(req, res) {
     console.log("add numbers");
     console.log(req.query);
     let num1 = req.params.num1;
-    let count = 0;
+    
    
-    const add = parseInt( num1) + req.query.num2;
-    res.status(200).send(add);
+    const add = parseInt( num1) + parseInt( req.query.num2);
+    console.log(add)
+    res.status(200).send(add+"");
 };
 
